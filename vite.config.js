0a0ba71764af75351ada75321557cee32f35fb25
Ample1827 <Adalromero99@gmail.com>
2025-04-18 15:02:39 -0600

    v2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio.github.io/",
+  base: "/Ample1827/Portfolio.github.io",
   plugins: [react(), tailwindcss()],
 });
