3e5eb477d8c58e7f2536651e893d2660ca132e2a
Ample1827 <Adalromero99@gmail.com>
2025-04-18 16:47:41 -0600

    vite updapte

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/",  // Changed to root
+  base: "./",  // Changed to root
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist",
     emptyOutDir: true,
+    sourcemap: true
   }
 });
